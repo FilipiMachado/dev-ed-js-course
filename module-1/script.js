4ea3388215e6956959a32139b258a25f657de260
Filipi <filipisee@hotmail.com>
2021-10-28 20:53:02 -0300

    small changes

--- module-1/script.js
+++ module-1/script.js
@@ -1,10 +1,22 @@
-const greet = () => {
-  let nameInput = prompt("What is your name?");
-  console.log(`Welcome to our website ${nameInput}`);
+/*
+const greet = (text) => {
+  console.log(`Welcome to our website ${text}`);
 };
 
-const signUp = ()=> {
-  greet();
-}
+const signUp = () => {
+  let nameInput = prompt("What is your name?");
+  greet(nameInput);
+};
 
 signUp();
+*/
+
+const maxNr = (n1, n2) => {
+  if (n1 > n2) {
+    return "n1 is bigger";
+  } else {
+    return "n2 is bigger";
+  }
+};
+
+console.log(maxNr(6, 5));
