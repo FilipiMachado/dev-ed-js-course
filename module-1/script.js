63e9225a09cafd0f8f0e71710f5bc0cb195e39d3
Filipi <filipisee@hotmail.com>
2021-10-28 21:46:19 -0300

    small changes

--- module-1/script.js
+++ module-1/script.js
@@ -24,5 +24,11 @@ const maxNr = (n1 = 6, n2 = 5) => {
 console.log(maxNr());
 */
 
+const usersList = ["Dev Fil", "Joao das Neves", "Severino", "Onça"];
+
+const numbers = [6, 5, 7.58742848484, 3.25];
+
+console.log(numbers.map(Math.round));
+console.log(numbers)
 
 
