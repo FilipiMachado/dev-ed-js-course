d780d78c5e52589c8d08efc2c48547d2e884ef59
fiil <filipisee@hotmail.com>
2021-11-08 22:38:17 -0300

    little changes

--- module_2/script.js
+++ module_2/script.js
@@ -1,6 +1,36 @@
-const nodeItems = document.querySelectorAll(".item");
-const todoList = document.querySelector(".todo-list");
-const todoNr = document.querySelectorAll(".todo-nr")[0];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const nodeItems = document.getElementsByClassName("item");
+const todoList = document.getElementById("todo-list");
+const todoNr = document.getElementsByClassName("todo-nr")[0];
 
 console.log(nodeItems);
 
@@ -9,9 +39,15 @@ newItem.classList.add("item");
 newItem.innerText = "Item 6";
 todoList.appendChild(newItem);
 
+const anotherItem = document.createElement("li");
+anotherItem.classList.add("item");
+anotherItem.innerText = "Item 7";
+todoList.appendChild(anotherItem);
+
 console.log(newItem);
 console.log(nodeItems);
 
 // Update the amount of items that we have
 
 todoNr.innerText = nodeItems.length;
+ */
