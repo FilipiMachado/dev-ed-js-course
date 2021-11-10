79810987dacad9026d1c2ebaaf42b74229745840
fiil <filipisee@hotmail.com>
2021-11-09 21:52:05 -0300

    small changes

--- module_2/events.js
+++ module_2/events.js
@@ -2,6 +2,7 @@ const button = document.querySelector("#submit-btn");
 const todoList = document.querySelector("#todo-list");
 const todoNr = document.querySelector(".todo-nr b");
 const mainTitle = document.querySelector(".main-title");
+const nameInput = document.querySelector(".name-input");
 
 /* button.addEventListener("click", () => {
   mainTitle.classList.toggle("title-change");
@@ -13,15 +14,26 @@ const items = todoList.children;
 
 // ATTACH A EVENT LISTENER
 button.addEventListener("click", () => {
+  // Creating element
   const newItem = document.createElement("li");
+  // Adding class
   newItem.classList.add("item");
+  // Adding the text
   newItem.innerText = `Item ${items.length + 1}`;
   todoList.appendChild(newItem);
   todoNr.innerText = items.length;
 
   // Create the element and attach the listener
   newItem.addEventListener("click", (event) => {
+    event.stopPropagation();
     event.target.remove();
     todoNr.innerText = items.length;
+
+    console.log(nameInput);
   });
+});
+
+todoList.addEventListener("click", () => {
+  console.log("The UL executes");
+  todoList.classList.toggle("fade");
 });
