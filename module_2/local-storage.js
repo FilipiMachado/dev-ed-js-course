b9c84d13024a7bf43d5c975411cfd9a8db92b3bc
fiil <filipisee@hotmail.com>
2021-11-10 21:32:18 -0300

    added local storage

--- module_2/local-storage.js
+++ module_2/local-storage.js
@@ -1,15 +1,22 @@
-// Local Storage
-
-localStorage.setItem("todo", "Feed the Cockatiel!");
+/* localStorage.setItem("todo", "Feed the Cockatiel!");
 localStorage.setItem("todo", "Rango pro Fil");
 localStorage.setItem("user", "Fil");
-
+ */
 /* // Session Storage
 
 sessionStorage.setItem("todoList", "Session feeding cat");
  */
 
-// Storing Stuff
-const user = localStorage.getItem("user");
+// Getting Stuff
+/* const user = localStorage.getItem("user");
 
 console.log(user);
+ */
+
+const myTodoList = ["feed the cockatiel", "wash", "listen to aot ost"];
+
+localStorage.setItem("todos", myTodoList);
+
+const retrieved = localStorage.getItem("todos");
+
+console.log(retrieved);
