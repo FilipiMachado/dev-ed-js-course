e29ca9e37e29b6585966163ef1435346b9ab2a84
fiil <filipisee@hotmail.com>
2021-11-13 18:36:12 -0300

    added addTask function

--- module_3/script.js
+++ module_3/script.js
@@ -1,11 +1,11 @@
 const taskInput = document.querySelector(".todo-form__input");
 const taskButton = document.querySelector(".todo-form__button");
-const taskButton = document.querySelector(".todo-form__button");
-
+const tasks = document.querySelector(".todo-form__task");
 
 taskButton.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log("huehuebr");
+  const newTask = tasks.value;
+  console.log(newTask);
 });
 
 console.log(taskInput.value);
