1e4a0246d04498525c1f58430ce51436d586b6dc
fiil <filipisee@hotmail.com>
2021-11-13 18:21:51 -0300

    added some elements to variables

--- module_3/script.js
+++ module_3/script.js
@@ -1,4 +1,9 @@
 const taskInput = document.querySelector(".todo-form__input");
+const taskButton = document.querySelector(".todo-form__button");
 
+taskButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log("huehuebr");
+});
 
 console.log(taskInput.value);
