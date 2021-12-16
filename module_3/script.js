37bf0e8b3665898674684ad48e75c21f7b0036aa
Filipi <filipisee@hotmail.com>
2021-12-16 19:40:33 -0300

    updating todo code

--- module_3/script.js
+++ module_3/script.js
@@ -11,18 +11,26 @@ taskButton.addEventListener("click", addTodo);
 
 function addTodo(event) {
   event.preventDefault();
+
   // Task Div
   const taskDiv = document.createElement("div");
   taskDiv.classList.add("todo");
+
   // New Task 
   const newTask = document.createElement("p")
   newTask.innerText = "hey"
   newTask.classList.add("task-item")
   taskDiv.appendChild(newTask);
+
   // Check Button
   const checkBtn = document.createElement("button")
   checkBtn.innerText = "V"
+  checkBtn.classList.add("check-btn")
+  taskDiv.appendChild(checkBtn)
+
   // Delete Button
   const deleteBtn = document.createElement("button")
-  checkBtn.innerText = "X"
+  deleteBtn.innerText = "X"
+  deleteBtn.classList.add("delete-btn")
+  taskDiv.appendChild(deleteBtn)
 }
